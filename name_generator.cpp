fb13c1a7426e37ae0c27809c52185e25553e3b40
Daniel Culliver <daniel.culliver@gmail.com>
2025-06-24 18:03:19 +0200

    Add command line options

--- name_generator.cpp
+++ name_generator.cpp
@@ -97,16 +97,86 @@ class NameGen {
 		}
 };
 
+int load_num(char *str) {
+	int num = 0;
+	for (int j = 0; str[j]; j++) {
+		if (str[j] < '0' || str[j] > '9') continue;
+		num *= 10;
+		num += str[j] - '0';
+	}
+	return num;
+}
+
+void print_help() {
+	std::cout << "===================================================================\n";
+	std::cout << "This program only supports single letter options.\n";
+	std::cout << "-h \tDisplays this help message.\n";
+	std::cout << "-m \tGenerates masculine names based on the generation count.\n";
+	std::cout << "-f \tGenerates feminine names based on the generation count.\n";
+	std::cout << "\t \tNote: if both -m and -f are active at once, then the total number of generated names will be twice the generation count.\n";
+	std::cout << "If neither are chosen, then every name has its gender chosen 50/50.\n";
+	std::cout << "-c [number] \tChanges the name generation count.\n";
+	std::cout << "-l [number] \tChanges the maximum length of prefixes to be considered during generation. The default value is 2.\n";
+	std::cout << "\t \tA higher number makes the name generated to be more similar to existing names, however it drastically increases the initial preparation time.";
+	std::cout << "-e [number] \tChanges the prefix-length exponent. The default value is 4 (i.e. every prefix has a weight of 2^[length].\n";
+	std::cout << "-b [number] \tChanges the base prefix multiplier. The default values is 65535. Decreasing this value can make names more random\n";
+	std::cout << "===================================================================\n";
+}
+
 int main(int argc, char *argv[]) {
+	bool masc = false;
+	bool fem = false;
+	int gen_count = 1;
+	uint8_t max_prefix_len = 2;
+	uint8_t len_prefix_mult = 4;
+	uint64_t base_prefix_mult = UINT16_MAX;
+
+	for (int i = 0; i < argc; i++) {
+		if (argv[i][0] == '-' && argv[i][1]) {
+			switch (argv[i][1]) {
+				case 'm':
+					masc = true;
+					break;
+				case 'f':
+					fem = true;
+					break;
+				case 'c':
+					gen_count = load_num(argv[++i]);
+					break;
+				case 'l':
+					max_prefix_len = load_num(argv[++i]);
+					break;
+				case 'e':
+					len_prefix_mult = load_num(argv[++i]);
+					break;
+				case 'b':
+					base_prefix_mult = load_num(argv[++i]);
+					break;
+				case 'h':
+					print_help();
+					return 0;
+					break;
+				default:
+					std::cout << "Error: Option not recognised\n";
+					print_help();
+					return 1;
+			}
+		}
+	}
+
 	NameGen gen;
 	std::string path = "./data/processed.csv";
-	gen.setup(path);
-	for (int j = 0; j < 5; j++) {
-		for (int i = 0; i < 10; i++) {
-			std::cout << gen.gen() << "\t";
-		}
-		std::cout << '\n';
+	gen.setup(path, max_prefix_len, len_prefix_mult, base_prefix_mult);
+	if (masc) {
+		for (int i = 0; i < gen_count; i++) std::cout << gen.gen_masc() << " ";
+		std::cout << std::endl;
 	}
-	std::cout << std::endl;
-	
+	if (fem) {
+		for (int i = 0; i < gen_count; i++) std::cout << gen.gen_fem() << " ";
+		std::cout << std::endl;
+	}
+	if (!masc && !fem) {
+		for (int i = 0; i < gen_count; i++) std::cout << gen.gen() << " ";
+		std::cout << std::endl;
+	}
 }
